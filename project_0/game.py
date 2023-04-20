a061936d11ea7381309c1b38bdfac4e993513c5b
Yana <yana_kus@icloud.com>
2023-04-20 22:22:44 +0500

    add bin

--- project_0/game.py
+++ project_0/game.py
@@ -1,18 +1,23 @@
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
 count = 0
-
+number = np.random.randint(1, 101)
+print("Загадано число от 1 до 100")
+min = 0
+max = 100
 while True:
+    predict = int(input())
     count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
+    if number == predict:
+        break
+    elif number > predict:
+        min = predict
+        print(f"Угадываемое число больше {predict}")
+        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max + min) / 2)}')
+    elif number < predict:
+        max = predict
+        print(f"Угадываемое число меньше {predict}")
+        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max+min)/2)}')
 
-    if predict_number > number:
-        print("Число должно быть меньше!")
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
-
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
+print(f"Вы угадали число {number} за {count} попыток.")
