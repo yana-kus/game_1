1d7a76e4dd44e89fe2cac49ab5e1ce9d51c4a284
Yana <yana_kus@icloud.com>
2023-04-20 22:45:27 +0500

    добавили алгоритм бинарного поиска

--- project_0/game.py
+++ project_0/game.py
@@ -1,23 +1,29 @@
+"""Игра угадай число"""
+
 import numpy as np
 
+number = np.random.randint(1, 101) # загадываем число
+pr_min=1
+pr_max=101
+# количество попыток
 count = 0
-number = np.random.randint(1, 101)
-print("Загадано число от 1 до 100")
-min = 0
-max = 100
+
 while True:
-    predict = int(input())
-    count += 1
-    if number == predict:
-        break
-    elif number > predict:
-        min = predict
-        print(f"Угадываемое число больше {predict}")
-        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max + min) / 2)}')
-    elif number < predict:
-        max = predict
-        print(f"Угадываемое число меньше {predict}")
-        print(f'Алгоритм бинарного поиска рекомендует вам число:{round((max+min)/2)}')
+    count+=1
+    predict_number = int(input("Угадай число от 1 до 100: "))
+    print("num",number)
+    
+    if predict_number > number:
+        pr_max=predict_number
+        print(f"Число должно быть меньше! Рекомендуем ввести: {round((pr_min+pr_max)/2)}")
+
+    elif predict_number < number:
+        pr_min=predict_number
+        print(F"Число должно быть больше! Рекомендуем ввести:{round((pr_min+pr_max)/2)}")
+    
+    else:
+        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+        break #конец игры выход из цикла
 
 
 print(f"Вы угадали число {number} за {count} попыток.")
